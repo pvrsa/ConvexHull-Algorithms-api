7052d1076c1427b7334809c93d4f7616a94a1f00
Sai Aasrith <sai.aasrith@gmail.com>
2018-01-30 22:28:07 +0530

    basic structure done

--- ConvexHull.cpp
+++ ConvexHull.cpp
@@ -8,3 +8,14 @@ ConvexHull::ConvexHull(){
 	cout << "Class object created\n";
 }
 
+void ConvexHull::grahamScan(){
+
+}
+
+void ConvexHull::jarvisMarch(){
+
+}
+
+void ConvexHull::ksAlgo(){
+
+}
--- ConvexHull.h
+++ ConvexHull.h
@@ -3,7 +3,9 @@ class ConvexHull
 {
 public:
 	ConvexHull();
-	
+	void grahamScan();
+	void jarvisMarch();
+	void ksAlgo();
 };
 
 
